4b712308dcb8a819b4ee3ca601c646c29db3b7e0
Duncan Moyo <duncanfmoyo@gmail.com>
2022-11-08 12:10:09 +0200

    icecream feature

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const store = require('./app/store')
-const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const store = require("./app/store");
+const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const iceCreamActions =
+  require("./features/icecream/icecreamSlice").iceCreamActions;
 
-console.log('initialState', store.getState());
+console.log("initialState", store.getState());
 
 const unsubscribe = store.subscribe(() => {
-console.log('updated Store', store.getState());
-})
+  console.log("updated Store", store.getState());
+});
 
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(3))
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
 
-unsubscribe()
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(2));
+
+unsubscribe();
