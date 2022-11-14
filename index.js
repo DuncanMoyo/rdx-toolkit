c7ba4ad4c794f658aefa5c11d1c61220cd8ea237
Duncan Moyo <duncanfmoyo@gmail.com>
2022-11-14 10:42:21 +0200

    Async Thunks

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const store = require("./app/store");
+const { fetchUsers } = require("./features/user/userSlice");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions =
   require("./features/icecream/icecreamSlice").iceCreamActions;
 
 console.log("initialState", store.getState());
+const fetchUser = require('./features/user/userSlice').fetchUsers
 
 const unsubscribe = store.subscribe(() => {
   console.log("Updated State: ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUser())
 
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(2));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-unsubscribe();
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(2));
+
+// unsubscribe();
